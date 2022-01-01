3912a0c063fb1373a3021544c96f4d32a6e5d08a
IC-Ilies <ilies@inovea-conseil.com>
2022-01-01 12:42:25 +0100

    Add sequelize + db support for heroku + user entity

---
+++ db.js
@@ -0,0 +1,57 @@
+
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const { Client } = require('pg')
+require('dotenv').config();
+
+let client, sequelize
+
+if(process.env.MODE == 'PROD'){
+  client = new Client({ 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false 
+    } 
+   });
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+}
+else{
+  client = new Client({
+    user : 'postgres',
+    host : 'localhost',
+    database : 'ressourcesrel',
+    port : 5432,
+    password : 'root'
+  });
+  sequelize = new Sequelize(
+    'ressourcesrel', 
+    'postgres', 
+    'root', 
+    {
+     host: 'localhost',
+     dialect: 'postgres'
+    }
+ ); 
+}
+
+/*   
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+ */
+
+module.exports = {
+    db: client,
+    sequelize: sequelize,
+    DataTypes: DataTypes
+}; 
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express =  require('express');  
+var userControler = require('../controller/userController');  
+
+// express.Router is a class to create route handlers  
+//router will contain the Router instance.  
+var router = express.Router();  
+  
+router.get('/',async (req,res)=>{
+    res.send(await userControler.fetchAll());
+}) 
+ 
+ router.get('/:id',async (req,res)=>{
+     res.send(await userControler.fetch(req.params.id));
+ })  
+  
+ router.post('/', async (req,res)=>{
+    
+     res.send( await userControler.create(req.body)); 
+    //res.send(await userControler.create(req.params));
+})  
+//this route will be executed on /user/add  
+//addUsers function will be called from the controller when request come for this route.  
+ //router.post('/add',userControler.addUsers)  
+  
+module.exports = router;  
