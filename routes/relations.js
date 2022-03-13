ea864ee0372c9df58ec881fa1647e3c9af371b26
Iliès BENYAHIA <ilies.benyahia@free>
2022-03-13 12:58:58 +0100

    ajout des types de relations

---
+++ routes/relations.js
@@ -0,0 +1,45 @@
+const express =  require('express');  
+const relationshipsController = require('../controller/ressourceRelationshipsController');
+// express.Router is a class to create route handlers  
+//router will contain the Router instance.  
+const router = express.Router();  
+
+
+/*
+
+router.get('/', ressourcesController.fetchAll);
+
+router.get('/:id', ressourcesController .fetch);
+
+*/
+
+// Ressources
+router.post('/', relationshipsController.create);
+router.get('/', relationshipsController.fetchAll);
+//router.get('/:id',relationshipsController.fetch);
+
+
+/*
+ router.get('/:id',async (req,res)=>{
+     res.send(await userController.fetch(req.params.id));
+ })
+ */
+
+/*
+ router.post('/', async (req,res)=>{
+    
+     res.send(await userController.create(req.body)); 
+    //res.send(await userController.create(req.params));
+})
+*/
+
+
+    //res.send(await userController.create(req.params));
+
+
+
+//this route will be executed on /user/add  
+//addUsers function will be called from the controller when request come for this route.  
+ //router.post('/add',userController.addUsers)  
+  
+module.exports = router;  
